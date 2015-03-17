2404cd5b0684ee01f3efb919122016078277889a
Maykel Moya <mmoya@mmoya.org>
2015-03-17 17:26:42 +0100

    Fix argument name

--- prometheus_client/__init__.py
+++ prometheus_client/__init__.py
@@ -42,7 +42,7 @@ class CollectorRegistry(object):
     with self._lock:
       self._collectors.add(collector)
 
-  def unregister(self, metric):
+  def unregister(self, collector):
     '''Remove a collector from the registry.'''
     with self._lock:
       self._collectors.remove(collector)
