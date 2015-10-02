ac880406cccb0f8ec206cccef5f7119e057ce956
Bernardo Sulzbach <mafagafogigante@gmail.com>
2015-10-02 13:00:26 -0300

    Fixed graphite.py formatting.
    This file had varying indentation levels.
    There was also an unused local.

--- prometheus_client/bridge/graphite.py
+++ prometheus_client/bridge/graphite.py
@@ -13,12 +13,12 @@ from .. import core
 # We also remove periods, so labels can be distinguished.
 _INVALID_GRAPHITE_CHARS = re.compile(r"[^a-zA-Z0-9_-]")
 
+
 def _sanitize(s):
-  return _INVALID_GRAPHITE_CHARS.sub('_', s)
-  
+    return _INVALID_GRAPHITE_CHARS.sub('_', s)
+
 
 class _RegularPush(threading.Thread):
-
     def __init__(self, pusher, interval):
         super(_RegularPush, self).__init__()
         self._pusher = pusher
@@ -30,15 +30,15 @@ class _RegularPush(threading.Thread):
             while True:
                 now = time.time()
                 if now >= wait_until:
-                   # May need to skip some pushes.
-                   while wait_until < now:
-                       wait_until += self._interval
-                   break
+                    # May need to skip some pushes.
+                    while wait_until < now:
+                        wait_until += self._interval
+                    break
                 # time.sleep can return early.
                 time.sleep(wait_until - now)
             try:
                 self._pusher.push()
-            except IOError as e:
+            except IOError:
                 logging.exception("Push failed")
 
 
@@ -50,27 +50,25 @@ class GraphiteBridge(object):
         self._time = _time
 
     def push(self):
-          now = int(self._time.time())
-          output = []
-          for metric in self._registry.collect():
-              for name, labels, value in metric._samples:
-                  if labels:
-                      labelstr = '.' + '.'.join(
-                          ['{0}.{1}'.format(
-                               _sanitize(k), _sanitize(v))
-                               for k, v in sorted(labels.items())])
-                  else:
-                      labelstr = ''
-                  output.append('{0}{1} {2} {3}\n'.format(
-                      _sanitize(name), labelstr, float(value), now))
+        now = int(self._time.time())
+        output = []
+        for metric in self._registry.collect():
+            for name, labels, value in metric._samples:
+                if labels:
+                    labelstr = '.' + '.'.join(
+                        ['{0}.{1}'.format(
+                            _sanitize(k), _sanitize(v))
+                         for k, v in sorted(labels.items())])
+                else:
+                    labelstr = ''
+                output.append('{0}{1} {2} {3}\n'.format(
+                    _sanitize(name), labelstr, float(value), now))
 
-          conn = socket.create_connection(self._address, self._timeout)
-          conn.sendall(''.join(output).encode('ascii'))
-          conn.close()
+        conn = socket.create_connection(self._address, self._timeout)
+        conn.sendall(''.join(output).encode('ascii'))
+        conn.close()
 
     def start(self, interval=60.0):
         t = _RegularPush(self, interval)
         t.daemon = True
         t.start()
-        
-
