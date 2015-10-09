e4f75b7e535544fd02669fca714d25a5d0d98d3c
Brian Brazil <brian.brazil@robustperception.io>
2015-10-09 16:15:19 +0100

    Release 0.0.13

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = "prometheus_client",
-    version = "0.0.12",
+    version = "0.0.13",
     author = "Brian Brazil",
     author_email = "brian.brazil@robustperception.io",
     description = ("Python client for the Prometheus monitoring system."),
