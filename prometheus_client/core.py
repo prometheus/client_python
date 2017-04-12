bb16ff39a206d6ae06427ce77c05ed0c99e4bd47
Алексей Бузанов <abuzanov@techops.ru>
2017-04-12 19:49:58 +0300

    Move os.getpid() inside _MmapedValue

--- prometheus_client/core.py
+++ prometheus_client/core.py
@@ -392,8 +392,7 @@ class _MmapedDict(object):
             self._f = None
 
 
-def _MultiProcessValue(__pid=os.getpid()):
-    pid = __pid
+def _MultiProcessValue(_pid=None):
     files = {}
     files_lock = Lock()
 
@@ -403,6 +402,8 @@ def _MultiProcessValue(__pid=os.getpid()):
         _multiprocess = True
 
         def __init__(self, typ, metric_name, name, labelnames, labelvalues, multiprocess_mode='', **kwargs):
+            pid = _pid or os.getpid()
+
             if typ == 'gauge':
                 file_prefix = typ + '_' +  multiprocess_mode
             else:
