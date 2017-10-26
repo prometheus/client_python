8c93850490a12649661768329cb761b85bfa19f0
Dhananjay Balan <dhananjay@cliqz.com>
2017-10-26 12:53:50 +0200

    Should fail if the multiproc path is not a directory.

--- tests/test_multiprocess.py
+++ tests/test_multiprocess.py
@@ -160,10 +160,26 @@ class TestMmapedDict(unittest.TestCase):
         os.unlink(self.tempfile)
 
 class TestUnsetEnv(unittest.TestCase):
-    def test_unset_syncdir_env(self):
+    def setUp(self):
         self.registry = CollectorRegistry()
+        fp, self.tmpfl = tempfile.mkstemp()
+        os.close(fp)
+
+    def test_unset_syncdir_env(self):
         self.assertRaises(
             ValueError,
             MultiProcessCollector,
             self.registry
         )
+
+    def test_file_syncpath(self):
+        registry = CollectorRegistry()
+        self.assertRaises(
+            ValueError,
+            MultiProcessCollector,
+            registry,
+            self.tmpfl
+        )
+
+    def tearDown(self):
+        os.remove(self.tmpfl)
