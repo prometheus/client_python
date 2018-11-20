1f5bee3f661f1a2a8a7e30522091b8e77d62be05
Girish Ranganathan <girish.rranganathan@gmail.com>
2018-11-20 12:22:20 -0500

    trying another sequence for negative test with python2
    Signed-off-by: Girish Ranganathan <girish.rranganathan@gmail.com>

--- tests/openmetrics/test_parser.py
+++ tests/openmetrics/test_parser.py
@@ -252,7 +252,7 @@ a_total{foo="bar",bar="b{a}z"} 1
             if sys.version_info >= (3,):
                 inj = u'\uD802'
             else:
-                inj = '\xc3\x83'
+                inj = '\xf8\xa1\xa1\xa1\xa1'
             families = text_string_to_metric_families('''# TYPE a counter
 # HELP a help
 a_total{foo="'''+inj+'''",bar="baz"} 1
