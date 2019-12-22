0c5b6117c2cb9a8ca453615d749b9e6a90a407c5
George Mossessian <gmossessian@gmail.com>
2019-12-22 16:26:01 -0500

    set http prefix in python geq 3.7.6
    Signed-off-by: George Mossessian <gmossessian@gmail.com>

--- prometheus_client/exposition.py
+++ prometheus_client/exposition.py
@@ -29,7 +29,8 @@ CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
 """Content type of the latest text format"""
 
 PYTHON26_OR_OLDER = sys.version_info < (2, 7)
-
+# Due to https://bugs.python.org/issue27657:
+PYTHON376_OR_NEWER = sys.version_info > (3, 7, 5)
 
 def make_wsgi_app(registry=REGISTRY):
     """Create a WSGI app which serves the metrics from a registry."""
@@ -341,7 +342,10 @@ def delete_from_gateway(
 
 def _use_gateway(method, gateway, job, registry, grouping_key, timeout, handler):
     gateway_url = urlparse(gateway)
-    if not gateway_url.scheme or (PYTHON26_OR_OLDER and gateway_url.scheme not in ['http', 'https']):
+    if not gateway_url.scheme or (
+            (PYTHON376_OR_NEWER or PYTHON26_OR_OLDER)
+            and gateway_url.scheme not in ['http', 'https']
+    ):
         gateway = 'http://{0}'.format(gateway)
     url = '{0}/metrics/{1}/{2}'.format(gateway, *_escape_grouping_key("job", job))
 
