228cff3592387b13d427891c75a3a61133bd71b2
Evan Yang <evan.yang@postmates.com>
2020-03-25 12:43:10 -0700

    I need to read up on eggs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="prometheus_client",
-    version="0.6.0",
+    version="0.7.1-alpha",
     author="Brian Brazil",
     author_email="brian.brazil@robustperception.io",
     description="Python client for the Prometheus monitoring system.",
