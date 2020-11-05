b939b065d5dce675e49d800e36cf94904095b9dc
Ben Kochie <superq@gmail.com>
2020-11-05 16:29:22 +0100

    Convert CI to CircleCI
    Replace Travis with CircleCI.
    * Update Python version list.
    
    Signed-off-by: Ben Kochie <superq@gmail.com>

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         'twisted': ['twisted'],
     },
     test_suite="tests",
-    python_requires=">=2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
@@ -39,7 +39,6 @@ setup(
         "Intended Audience :: System Administrators",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
